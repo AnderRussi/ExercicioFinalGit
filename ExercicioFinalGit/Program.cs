f417533aa8b281f0f3016104c522ef6abadb5f08
AnderRussi <andersonrussi.geol@gmail.com>
2025-06-04 19:43:25 -0300

    projeto inicial

--- ExercicioFinalGit/Program.cs
+++ ExercicioFinalGit/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// See https://aka.ms/new-console-template for more information
+//declaração das variáveis
+int idade, sexo;
+
+//comandos de entrada e leitura de dados
+Console.WriteLine("Informe a idade da pessoa: ");
+idade = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("\nInforme o sexo da pessoa, sendo 1- Masculino 2-Feminino: ");
+sexo = Convert.ToInt32(Console.ReadLine());
+
+//Estrutura de seleção
+//Se a idade for maior ou igual a 18
+if (idade >= 18)
+{
+    //Bloco verdade
+    //Verifica o sexo, se for igual 1
+    if (sexo == 1)
+    {
+        //Bloco verdade
+        Console.WriteLine("Você deve se alistar no exército!");
+    }
+    else // se não
+    {
+        //Bloco Falso
+        Console.WriteLine("Você não se enquadra para alistamento militar");
+    }
+}
+else //se não
+{
+    //Bloco falso
+    Console.WriteLine("Você não tem idade suficiente para alistamento militar!");
+}
